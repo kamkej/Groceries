e6f5e4a451e9211ef8b7333386c8885d5f87e26b
Julio Kamke <julio@thor.mgmt.gvt.net.br>
2017-07-28 16:40:03 -0300

    cap4

--- platforms/android/src/main/assets/app/views/login/login.js
+++ platforms/android/src/main/assets/app/views/login/login.js
@@ -1,20 +1,28 @@
 var frameModule = require("ui/frame");
-var ObeservableModule = require("data/observable");
+var UserViewModel = require("../../shared/view-models/user-view-model");
+var dialogsModule = require("ui/dialogs");
+var user = new UserViewModel();
 var page;
 var email;
-var user = new ObeservableModule.fromObject({
-    email : "user@domain.com",
-    password: "password"
-});
+
 exports.loaded = function(args){
     page = args.object;
     page.bindingContext = user;
 }
-exports.signIn = function(){
-    email = page.getViewById("email");
-    console.log(email.text);
-
-}
+exports.signIn = function() {
+    user.login()
+        .catch(function(error) {
+            console.log(error);
+            dialogsModule.alert({
+                message: "Unfortunately we could not find your account.",
+                okButtonText: "OK"
+            });
+            return Promise.reject();
+        })
+        .then(function() {
+            frameModule.topmost().navigate("views/list/list");
+        });
+};
 exports.register = function(){
     var topmost = frameModule.topmost();
     topmost.navigate("views/register/register");
